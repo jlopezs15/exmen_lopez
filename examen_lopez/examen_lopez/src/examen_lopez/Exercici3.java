3e75e3f6a125fa0f39d214bfe1ec0f51f1bdbb99
jlopezs15 <59867702+jlopezs15@users.noreply.github.com>
2020-02-28 09:25:30 +0100

    Add files via upload
    Esborrar el metode printwo

---
+++ examen_lopez/examen_lopez/src/examen_lopez/Exercici3.java
@@ -0,0 +1,14 @@
+package examen_lopez;
+
+public class Exercici3 {
+
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+	
+}
