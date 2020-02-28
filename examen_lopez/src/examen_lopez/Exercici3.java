1cea93eb88527669f81ca28aabebbc3d83297053
jlopezs15 <59867702+jlopezs15@users.noreply.github.com>
2020-02-28 09:21:43 +0100

    Add files via upload

---
+++ examen_lopez/src/examen_lopez/Exercici3.java
@@ -0,0 +1,20 @@
+package examen_lopez;
+
+public class Exercici3 {
+
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+
+	}
+}
